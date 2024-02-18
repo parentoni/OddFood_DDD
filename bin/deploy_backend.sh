0f31af35d735ad3599e2a3beeeeb26340f561260
arthuroi <parentoni.arthur@gmail.com>
2024-02-18 16:22:23 -0300

    feat: Added bin files

---
+++ bin/deploy_backend.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ../backend/.config/server/.env
+
+echo "[DEPLOY_BACKEND]: Selected Host: $HOSTNAME"
+
+# Build backend
+rm -rf ../backend/dist
+npm run --silent build --prefix ../backend
+
+echo "[DEPLOY_BACKEND]: Typescript compiled"
+
+# Copy necessary files to instance
+
+ssh $USER@$HOSTNAME -i $IDENTITYFILE 'rm -rf app && mkdir app'
+
+scp  -q -i $IDENTITYFILE -r ./remote $USER@$HOSTNAME:~/. # Copy scripts
+scp  -q -i $IDENTITYFILE -r ../backend/.config/nginx $USER@$HOSTNAME:~/. # Copy scripts
+scp  -q -i $IDENTITYFILE -r ../backend/dist $USER@$HOSTNAME:~/app # Copy build
+scp  -q -i $IDENTITYFILE -r ../backend/static $USER@$HOSTNAME:~/app # Copy static
+scp  -q -i $IDENTITYFILE -r ../backend/package*.json $USER@$HOSTNAME:~/app # Copy package*
+scp  -q -i $IDENTITYFILE -r ../backend/pm2.json $USER@$HOSTNAME:~/app # Copy pm2 configs
+
+echo "[DEPLOY_BACKEND]: Copied source code to host machine."
+# todo: copy build
+
+ssh $USER@$HOSTNAME -i $IDENTITYFILE 'sudo chmod +x ~/remote/startup.sh && ./remote/startup.sh '
